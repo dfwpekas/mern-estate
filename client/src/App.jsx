7fe817c2825b056512df9b7596b802aefbfea8bc
dfwpekas <chiemerienwafor88@gmail.com>
2024-11-29 15:18:53 +0100

    updated the routes and made it active alongside creating and adding the header component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'> App </h1>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='*' element={<div>Not Found</div>}/>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
